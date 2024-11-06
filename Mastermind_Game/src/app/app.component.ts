6d2d8c3b543321a4e8d4fe620ea0c7e5f1295b1c
Vipp90 <g.zatorski90@wp.pl>
2024-11-06 17:19:17 +0100

    Setup project

---
+++ Mastermind_Game/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<h1>Welcome to {{title}}!</h1>`,
+
+})
+export class AppComponent {
+  title = 'Mastermind_Game';
+}
