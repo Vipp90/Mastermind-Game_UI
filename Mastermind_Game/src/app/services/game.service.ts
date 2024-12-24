872468b04189483c5535dcb3ade823e6af60e30c
Vipp90 <g.zatorski90@wp.pl>
2024-12-24 21:00:58 +0100

    add saveScore and deleteGame method

--- Mastermind_Game/src/app/services/game.service.ts
+++ Mastermind_Game/src/app/services/game.service.ts
@@ -10,23 +10,38 @@ import { CheckCodeResponse } from "../models/CheckCodeResponse";
 @Injectable({ providedIn: "root" })
 export class GameService {
   private readonly apiUrl = environment.apiUrl;
+  private readonly headers = new HttpHeaders({ "Content-Type": "application/json" });
   private http = inject(HttpClient);
 
+  private getHttpOptions() {
+    return { headers: this.headers };
+  }
+
   createGame(gameInfo: GameInfo): Observable<ApiResponse<string>> {
-    const headers = new HttpHeaders({ "Content-Type": "application/json" });
-    return this.http.post<ApiResponse<string>>(this.apiUrl + "/game", gameInfo, {
-      headers,
-    });
+    return this.http.post<ApiResponse<string>>(
+      this.apiUrl + "/game",
+      gameInfo,
+      this.getHttpOptions()
+    );
   }
 
   checkCode(gameId: string, code: Code): Observable<ApiResponse<CheckCodeResponse>> {
-    const headers = new HttpHeaders({ "Content-Type": "application/json" });
     return this.http.post<ApiResponse<CheckCodeResponse>>(
       this.apiUrl + `/game/${gameId}/guess`,
       code,
-      {
-        headers,
-      }
+      this.getHttpOptions()
     );
+  }
+
+  saveScore(gameId: string, score: number): Observable<ApiResponse<string>> {
+    return this.http.post<ApiResponse<string>>(
+      this.apiUrl + `/game/${gameId}/score`,
+      score,
+      this.getHttpOptions()
+    );
+  }
+
+  deleteGame(gameId: string): Observable<void> {
+    return this.http.delete<void>(this.apiUrl + `/game/${gameId}`);
   }
 }
