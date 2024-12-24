b5e5853326d1ffbbce3695f656d02437a98ea6ec
Vipp90 <g.zatorski90@wp.pl>
2024-12-24 21:01:53 +0100

    modify notificationService

--- Mastermind_Game/src/app/services/notification.service.ts
+++ Mastermind_Game/src/app/services/notification.service.ts
@@ -3,13 +3,40 @@ import { ToastrService } from "ngx-toastr";
 
 @Injectable({ providedIn: "root" })
 export class NotificationService {
+  private isToastActive = false;
+  private toastQueue: {
+    message: string;
+    title?: string;
+    type: "success" | "error" | "info" | "warning";
+  }[] = [];
   constructor(private toastr: ToastrService) {}
 
-  showError(message: string) {
-    this.toastr.error(message, "", {
-      timeOut: 5000,
-      progressBar: true,
-      closeButton: true,
-    });
+  showToast(
+    type: "success" | "error" | "info" | "warning",
+    message: string,
+    title?: string
+  ) {
+    this.toastQueue.push({ type, message, title });
+    this.processQueue();
+  }
+
+  private processQueue() {
+    if (this.isToastActive || this.toastQueue.length === 0) {
+      return;
+    }
+
+    this.isToastActive = true;
+    const toast = this.toastQueue.shift();
+
+    if (toast) {
+      this.toastr[toast.type](toast.message, toast.title, {
+        timeOut: 4000,
+        progressBar: true,
+        closeButton: true,
+      }).onHidden.subscribe(() => {
+        this.isToastActive = false;
+        this.processQueue();
+      });
+    }
   }
 }
